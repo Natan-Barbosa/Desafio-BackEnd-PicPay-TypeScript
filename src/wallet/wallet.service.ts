bb7fc820493720fd8033f16adfec474820b5eb65
natan <natanbarbosax193@gmail.com>
2025-07-18 20:22:46 -0300

    edit: add new service

--- src/wallet/wallet.service.ts
+++ src/wallet/wallet.service.ts
@@ -1,4 +1,4 @@
-import { Injectable } from '@nestjs/common';
+import { BadRequestException, Injectable } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { WalletEntity } from 'src/Entities/WalletEntity/wallet.entity';
 import { Repository } from 'typeorm';
@@ -16,5 +16,13 @@ export class WalletService {
     const entity = plainToInstance(WalletEntity, data);
     const createdWallet = await this.walletRepository.save(entity);
     return createdWallet;
+  }
+
+  async getWalletById(id: string) {
+    const findedWallet = await this.walletRepository.findOneBy({ id });
+    if (findedWallet == null) {
+      throw new BadRequestException('Wallet Not Found, Verify The Sent Data');
+    }
+    return findedWallet;
   }
 }
