160559c29e24190e31d374fa5a659bbcfad0c8cb
natan <natanbarbosax193@gmail.com>
2025-07-18 20:22:34 -0300

    edit: add new endpoint

--- src/wallet/wallet.controller.ts
+++ src/wallet/wallet.controller.ts
@@ -1,6 +1,7 @@
-import { Body, Controller, Post } from '@nestjs/common';
+import { Body, Controller, Get, Param, Post } from '@nestjs/common';
 import { WalletService } from './wallet.service';
 import { WalletCreateDto } from './dto/wallet.create.dto';
+import { FindByIdDto } from './dto/wallet.findbyid.dto';
 
 @Controller('wallet')
 export class WalletController {
@@ -9,5 +10,11 @@ export class WalletController {
   @Post()
   async createWallet(@Body() body: WalletCreateDto) {
     return await this.walletService.createWallet(body);
+  }
+
+  @Get(':id')
+  async getWalletById(@Param() params: FindByIdDto) {
+    const serviceResponse = await this.walletService.getWalletById(params.id);
+    return serviceResponse;
   }
 }
