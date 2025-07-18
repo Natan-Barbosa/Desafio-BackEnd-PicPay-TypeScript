5da90b670fa2e079406ccea33dbda485ce8891c4
natan <natanbarbosax193@gmail.com>
2025-07-18 19:54:56 -0300

    edit: add wallet module and auto load entities instruct

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,6 +2,7 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { TypeOrmModule } from '@nestjs/typeorm';
+import { WalletModule } from './wallet/wallet.module';
 import * as dotenv from 'dotenv';
 
 dotenv.config();
@@ -16,8 +17,10 @@ dotenv.config();
       password: process.env.DB_PASSWORD,
       database: process.env.DB_DATABASE,
       entities: [__dirname + '/**/*.entity{.ts,.js}'], // Search All Entities in directories
+      autoLoadEntities: true, //Load registered entities in application
       synchronize: true, //Não Usar Em Produção Para Não Apagar As Tabelas
     }),
+    WalletModule,
   ],
   controllers: [AppController],
   providers: [AppService],
