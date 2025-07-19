f60fe1f16c0eee3cf1be29b7be7d38e4f4d787e6
natan <natanbarbosax193@gmail.com>
2025-07-19 17:07:45 -0300

    add: wallet increase balance dto

---
+++ src/wallet/dto/wallet.increase.dto.ts
@@ -0,0 +1,29 @@
+import {
+  IsNotEmpty,
+  IsNumber,
+  IsPositive,
+  IsString,
+  MaxLength,
+  MinLength,
+} from 'class-validator';
+import { walletType } from 'src/Entities/WalletEntity/wallet.enum';
+
+export class IncreaseBalanceDto {
+  @IsNotEmpty({ message: 'Field Cannot Be Empty' })
+  @IsString({ message: 'Field Must Be String' })
+  @MinLength(11)
+  @MaxLength(14)
+  cpfOrCnpj: string;
+
+  @IsNotEmpty({ message: 'Field Cannot Be Empty' })
+  @IsString({ message: 'Field Must Be String' })
+  password: string;
+
+  @IsNotEmpty({ message: 'Field Cannot Be Empty' })
+  @IsString({ message: 'Field Must Be String' })
+  walletType: walletType;
+
+  @IsPositive({ message: 'Field Cannot Be Negative Or Zero' })
+  @IsNumber({ allowInfinity: false, allowNaN: false })
+  value: number;
+}
