e9e60e9c95c77dc949f315d155d940b48bd4d53e
natan <natanbarbosax193@gmail.com>
2025-07-19 17:06:11 -0300

    edit: add increase wallet endpoint

--- src/wallet/wallet.controller.ts
+++ src/wallet/wallet.controller.ts
@@ -1,7 +1,8 @@
-import { Body, Controller, Get, Param, Post } from '@nestjs/common';
+import { Body, Controller, Get, Param, Post, Put } from '@nestjs/common';
 import { WalletService } from './wallet.service';
 import { WalletCreateDto } from './dto/wallet.create.dto';
 import { FindByIdDto } from './dto/wallet.findbyid.dto';
+import { IncreaseBalanceDto } from './dto/wallet.increase.dto';
 
 @Controller('wallet')
 export class WalletController {
@@ -15,6 +16,12 @@ export class WalletController {
   @Get(':id')
   async getWalletById(@Param() params: FindByIdDto) {
     const serviceResponse = await this.walletService.getWalletById(params.id);
+    return serviceResponse;
+  }
+
+  @Put()
+  async increaseWalletBalance(@Body() body: IncreaseBalanceDto) {
+    const serviceResponse = await this.walletService.IncreaseBalance(body);
     return serviceResponse;
   }
 }
