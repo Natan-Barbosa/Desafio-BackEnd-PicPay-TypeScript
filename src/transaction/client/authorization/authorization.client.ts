c5953785c4291ad53ce4c7676f5fba499f0e8286
natan <natanbarbosax193@gmail.com>
2025-07-21 11:13:05 -0300

    edit: add logger

--- src/transaction/client/authorization/authorization.client.ts
+++ src/transaction/client/authorization/authorization.client.ts
@@ -1,16 +1,21 @@
 import { HttpService } from '@nestjs/axios';
-import { Injectable, UnauthorizedException } from '@nestjs/common';
+import { Injectable, Logger, UnauthorizedException } from '@nestjs/common';
 
 @Injectable()
 export class AuthorizationClient {
+  private readonly logger = new Logger(AuthorizationClient.name);
+
   constructor(private readonly httpService: HttpService) {}
 
   public async isAuthorized() {
     await this.httpService.axiosRef
       .get('https://util.devi.tools/api/v2/authorize')
       .then((data) => data)
-      .catch((error) => {
-        throw new UnauthorizedException(error);
+      .catch((error: Error) => {
+        this.logger.error(error.message);
+        throw new UnauthorizedException(
+          'You Are Not Authorized To Realize Transaction',
+        );
       });
   }
 }
