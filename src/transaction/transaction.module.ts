a1c44cce41e37cfbd8e1b052de3e310fed36506d
natan <natanbarbosax193@gmail.com>
2025-07-21 11:12:28 -0300

    edit: add notification module

--- src/transaction/transaction.module.ts
+++ src/transaction/transaction.module.ts
@@ -4,9 +4,14 @@ import { TransactionController } from './transaction.controller';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { WalletEntity } from 'src/wallet/entity/wallet.entity';
 import { AuthorizationModule } from './client/authorization/authorization.module';
+import { NotificationModule } from './client/notification/notification.module';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([WalletEntity]), AuthorizationModule],
+  imports: [
+    TypeOrmModule.forFeature([WalletEntity]),
+    AuthorizationModule,
+    NotificationModule,
+  ],
   controllers: [TransactionController],
   providers: [TransactionService],
 })
